693636d91cd3e4eaeff3bd81b66353797ecd5f2e
Philip Holm Møller <phmo61881@edu.ucl.dk>
2025-04-11 17:52:27 +0200

    added reference and model classes

---
+++ TanzEksp/Shared/Domain/Zipcode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TanzEksp.Shared.Domain
+{
+    public class Zipcode
+    {
+    }
+}
