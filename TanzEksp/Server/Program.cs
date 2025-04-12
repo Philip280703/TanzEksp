d7dc780a5f23a819e78386e2260127f158798fad
Philip Holm Møller <phmo61881@edu.ucl.dk>
2025-04-12 20:56:40 +0200

    manglede ref til client fra server, bad practice men nødvendigt

--- TanzEksp/Server/Program.cs
+++ TanzEksp/Server/Program.cs
@@ -12,10 +12,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 
+builder.Services.AddServerServices(); // Register IOC service her
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddServerServices(); // Register IOC service her
 
 var app = builder.Build();
 
