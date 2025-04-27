e49178871a03ccb874aab5bcee1bdad145478240
Andreas Fiil Thomsen <afth61680@edu.ucl.dk>
2025-04-27 16:53:41 +0200

    Validering på Kunderoversigt

--- TanzEksp/Shared/DTO/CustomerDTO.cs
+++ TanzEksp/Shared/DTO/CustomerDTO.cs
@@ -9,16 +9,20 @@ namespace TanzEksp.Shared.DTO
 {
     public class CustomerDTO
     {
-       
         public int Id { get; set; }
 
-        public string FirstName { get; set; } = "";
+        [Required(ErrorMessage = "Fornavn er påkrævet.")]
+        public string? FirstName { get; set; }
 
-        public string LastName { get; set; } = "";
+        [Required(ErrorMessage = "Efternavn er påkrævet.")]
+        public string? LastName { get; set; }
 
-        public string Email { get; set; } = "";
+        [Required(ErrorMessage = "Email er påkrævet.")]
+        [EmailAddress(ErrorMessage = "Indtast en gyldig email-adresse.")]
+        public string? Email { get; set; }
 
-        public string PhoneNumber { get; set; } = "";
-
+        [Required(ErrorMessage = "Telefonnummer er påkrævet.")]
+        [Phone(ErrorMessage = "Indtast et gyldigt telefonnummer.")]
+        public string? PhoneNumber { get; set; }
     }
 }
