ab93513503c80250d3ec238866d28c38e3b79ec2
Philip Holm Møller <phmo61881@edu.ucl.dk>
2025-05-14 17:10:18 +0200

    update pdf

--- TanzEksp/Server/Helpers/PdfGenerator.cs
+++ TanzEksp/Server/Helpers/PdfGenerator.cs
@@ -15,6 +15,8 @@ namespace TanzEksp.Server.Helpers
         private readonly List<DayPlanDTO> dayplans;
         private readonly BookingDTO booking;
 
+        int dayCount = 1;
+
         public PdfGenerator(CustomerDTO customer, List<TripEventDTO> tripEvents, List<DayPlanDTO> dayPlans, BookingDTO Booking)
         {
             Customer = customer;
@@ -38,9 +40,8 @@ namespace TanzEksp.Server.Helpers
                 {
                     row.RelativeItem().Column(col =>
                     {
-                        col.Item().Text("Rejseplanen til dit livs oplevelse").FontSize(20).Bold().FontColor(Colors.Blue.Medium);
+                        col.Item().Text("Rejseplanen til dit livs oplevelse").FontSize(20).Bold().FontColor(Colors.Orange.Medium);
                         col.Item().Text($"Booking Nr: {booking.Id}").FontSize(10).FontColor(Colors.Grey.Darken2);
-                        col.Item().Text($"Rejsene: voksne: {booking.AdultCount}, børn {booking.ChildCount}");
                     });
 
                     row.ConstantItem(100).Height(60).Image(logoPath, ImageScaling.FitArea);
@@ -51,9 +52,7 @@ namespace TanzEksp.Server.Helpers
                     // Kundeoplysninger
                     col.Item().PaddingBottom(10).Text($"Kunde: {Customer.FirstName} {Customer.LastName}").FontSize(12);
                     col.Item().Text($"Email: {Customer.Email}").FontSize(12);
-                    col.Item().Text($"Telefon: {Customer.PhoneNumber}").FontSize(12);
-                    col.Item().Text($"Antal voksne: {booking.AdultCount}").FontSize(12);
-                    col.Item().Text($"Antal børn: {booking.ChildCount}").FontSize(12);
+                    col.Item().Text($"Antal: {booking.AdultCount} + {booking.ChildCount}").FontSize(12);
                     col.Item().Text($"Tilbud genereret: {DateTime.Now:dd. MMMM yyyy}").FontSize(12);
 
                     col.Item().PaddingVertical(20).LineHorizontal(1).LineColor(Colors.Grey.Lighten2);
@@ -61,9 +60,9 @@ namespace TanzEksp.Server.Helpers
                     foreach (var evt in tripevents)
                     {
 
-                        int dayCount = 1;
+                       
                         col.Item().Text($"Rejse: {evt.Title}")
-                            .FontSize(16).Bold().FontColor(Colors.Blue.Darken2);
+                            .FontSize(16).Bold().FontColor(Colors.Orange.Darken1);
 
                         col.Item().Container().PaddingBottom(10)
                         .Text(evt.Description ?? "Beskrivelse ikke angivet").FontSize(12);
@@ -77,7 +76,7 @@ namespace TanzEksp.Server.Helpers
                             {
                                 col.Item().PaddingBottom(5).Column(dayCol =>
                                 {
-                                    dayCol.Item().Text($"Dag {dayCount}: {day.Title}").Bold().FontSize(12).FontColor(Colors.Blue.Medium);
+                                    dayCol.Item().Text($"Dag {dayCount}: {day.Title}").Bold().FontSize(12).FontColor(Colors.Orange.Accent2);
                                     dayCol.Item().Text(day.Description).FontSize(11);
                                     if (!string.IsNullOrWhiteSpace(day.Meals))
                                         dayCol.Item().Container().PaddingTop(5).Text($"Måltider: {day.Meals}").FontSize(12).Italic();
