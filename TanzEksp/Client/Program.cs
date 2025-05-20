bd5f597a68fe1dcae4989d4ec04ae488a1b253ba
MarkKochRuge <165993382+MarkKochRuge@users.noreply.github.com>
2025-05-20 11:24:14 +0200

    tilføjet MessageHandler til programcs

--- TanzEksp/Client/Program.cs
+++ TanzEksp/Client/Program.cs
@@ -15,6 +15,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddTransient<CustomAuthorizationMessageHandler>();
+
 builder.Services.AddScoped(sp =>
 {
     var localStorage = sp.GetRequiredService<ILocalStorageService>();
